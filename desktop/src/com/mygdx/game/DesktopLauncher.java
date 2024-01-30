4c47c6a24ab7ed16dfb1ea6664ab179af4e9d65a
Jonber0404 <jonber0404@skola.goteborg.se>
2024-01-30 12:28:49 +0100

    t

--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -2,14 +2,15 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.mygdx.game.MyGdxGame;
+import com.mygdx.game.JumpyBirb;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setTitle("My GDX Game");
-		new Lwjgl3Application(new MyGdxGame(), config);
+		config.setTitle("JumpyBirbGrupp2");
+		new Lwjgl3Application(new JumpyBirb(), config);
+		System.out.println("test");
 	}
 }
