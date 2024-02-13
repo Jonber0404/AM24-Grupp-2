621c0fa8f01adbf9d8a4fbd78bec16234db0bab8
Adam Paajarvi <adampaajarvi@gmail.com>
2024-02-13 08:51:55 +0100

    Adding a Prototype Animator.java file that may be used for future animations or may be deleted

---
+++ core/src/com/mygdx/game/Animator.java
@@ -0,0 +1,33 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class Animator implements ApplicationListener {
+    private static final int FRAME_COLS = 6, FRAME_ROWS = 5;
+    public Texture birbSheet;
+    SpriteBatch spriteBatch;
+
+    public Animation<TextureRegion> flappyAnimation;
+    public Animation<TextureRegion> jumpyAnimation;
+
+    flappyAnimation = new Animation<TextureRegion>(0.033f, atlas.findRegions("running"), Animation.PlayMode.LOOP);
+    jumpyAnimation = new Animation<TextureRegion>(0.033f, atlas.findRegions("running", Animation.PlayMode.LOOP));
+
+    //En variable för att hålla koll på den förflutna tiden
+    float stateTime;
+
+    @Override
+    public void create(){
+        birbSheet = new Texture(Gdx.files.internal("sheetname.png"));	//insert actual name.
+
+        // Använder split utility metoden för att skapa en 2D array av TextureRegions. Detta är
+        // möjligt för att denna sprite sheet har frames av samma storlek och de är alla justerade.
+        TextureRegion[][] tmp = TextureRegion.split(birbSheet, birbSheet.getWidth() / FRAME_COLS,
+                birbSheet.getHeight() / FRAME_ROWS);
+    }
+}
